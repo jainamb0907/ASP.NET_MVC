826c12ec7e790396e81ac36e0ef3aacd8dcefd46
jainam.b <jainam.b@simformsolutions.com>
2023-06-19 14:34:43 +0530

    Practical14
    Implemented Searching & sorting, and pagination with CRUD Operation in ASP.NET

---
+++ Practical13/Practical-13/Practical-13/Migrations/202306061035495_final.cs
@@ -0,0 +1,24 @@
+﻿namespace Practical_13.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class final : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.user", "FirstName", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.user", "MiddleName", c => c.String(maxLength: 50));
+            AlterColumn("dbo.user", "LastName", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.user", "Address", c => c.String(maxLength: 100));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.user", "Address", c => c.String());
+            AlterColumn("dbo.user", "LastName", c => c.String(nullable: false));
+            AlterColumn("dbo.user", "MiddleName", c => c.String());
+            AlterColumn("dbo.user", "FirstName", c => c.String(nullable: false));
+        }
+    }
+}
