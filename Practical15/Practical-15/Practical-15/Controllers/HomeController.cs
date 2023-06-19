e6209c15c47949d1dfb2865ee36a6ac3167e0af2
jainam.b <jainam.b@simformsolutions.com>
2023-06-19 15:02:40 +0530

    Practical15
    Implemented Windows authentication & Form authentication

---
+++ Practical15/Practical-15/Practical-15/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practical_15.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Test01()
+        {
+            return View();
+        }
+    }
+}
